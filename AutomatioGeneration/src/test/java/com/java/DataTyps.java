e08dd81318412dcff888f52c194793d25bd78fb0
flacoaaron <aaron_mega84@hotmail.com>
2021-01-23 12:26:13 -0600

    tipos de datos

---
+++ AutomatioGeneration/src/test/java/com/java/DataTyps.java
@@ -0,0 +1,39 @@
+package com.java;
+
+public class DataTyps {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//variables númericas//
+		byte variablebyte = 127;
+		
+		int variableint = 128;
+		
+		double altura = 2.54;
+		
+		//variables caracter y logico//
+		
+		char letra = '\u0048';
+		String varstring = "esto es string";
+		
+		//dato booleano falso o verdadero
+		boolean isDog = true;
+		
+		System.out.println(variablebyte);
+		System.out.println(variableint);
+		
+		System.out.println("altura: " + altura);
+		System.out.println("imprimir char " + letra);
+		System.out.println("variable string" + varstring);
+		System.out.println("imprimir booleano "+ isDog);
+		
+		//setear variable
+		
+		letra = '\u0118';
+		
+		System.out.println("Imprimir variable seteada "+ letra);
+		
+
+	}
+
+}
