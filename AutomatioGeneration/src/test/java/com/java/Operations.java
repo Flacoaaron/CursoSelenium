da9d9d10e03120d74eaaeae0cd92472c9054ee19
flacoaaron <aaron_mega84@hotmail.com>
2021-01-31 22:18:02 -0600

    operadores 1

---
+++ AutomatioGeneration/src/test/java/com/java/Operations.java
@@ -0,0 +1,60 @@
+package com.java;
+
+public class Operations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//aritmeticos.
+		int x = 2;
+		int y = 3;
+		
+		int z = x * y;
+		
+		System.out.println("la suma es: " + z);
+		
+		//operadore uniarios
+		int A = 2;
+		int B = 2;
+		
+//		System.out.println(-A);
+//		System.out.println(--A);
+//		System.out.println(-B);
+//		System.out.println(--B);
+//		System.out.println(+A);
+//		System.out.println(++A);
+//		System.out.println(+B);
+		System.out.println(++B);
+		
+		//Relacionales
+		
+		int C = 2;
+		int D = 2;
+		
+		if (C != D) {
+			
+			System.out.println("C y D son Diferentyes");
+		}
+		
+		if (C == D) {
+			
+			System.out.println("C y D son iguales");
+		}
+		
+		if (C < D) {
+			
+			System.out.println("C es menor aDB ");
+		}
+		
+	
+		if (C >= D) {
+			
+			System.out.println("C mayor o igual a D ");
+		}
+		
+		
+
+		
+	}
+
+}
